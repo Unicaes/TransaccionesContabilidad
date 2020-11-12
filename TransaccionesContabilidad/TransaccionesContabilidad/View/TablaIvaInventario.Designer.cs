34f269dee7f441d449eb0d4f471b2f494246cfec
Bryan Palma <bryanmauriciopalma@hotmail.com>
2020-11-11 19:29:19 -0600

    tabla 1.2 finalizada

---
+++ TransaccionesContabilidad/TransaccionesContabilidad/View/TablaIvaInventario.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace TransaccionesContabilidad.View
+{
+    partial class TablaIvaInventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.concepto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mov_deudor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mov_acree = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cod,
+            this.concepto,
+            this.mov_deudor,
+            this.mov_acree});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 11);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(787, 602);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // cod
+            // 
+            this.cod.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.cod.FillWeight = 100.1218F;
+            this.cod.HeaderText = "Código";
+            this.cod.MinimumWidth = 6;
+            this.cod.Name = "cod";
+            this.cod.ReadOnly = true;
+            this.cod.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.cod.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // concepto
+            // 
+            this.concepto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.concepto.FillWeight = 100.0962F;
+            this.concepto.HeaderText = "Concepto";
+            this.concepto.MinimumWidth = 6;
+            this.concepto.Name = "concepto";
+            this.concepto.ReadOnly = true;
+            this.concepto.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.concepto.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // mov_deudor
+            // 
+            this.mov_deudor.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.mov_deudor.FillWeight = 99.34443F;
+            this.mov_deudor.HeaderText = "Debe";
+            this.mov_deudor.MinimumWidth = 6;
+            this.mov_deudor.Name = "mov_deudor";
+            this.mov_deudor.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.mov_deudor.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // mov_acree
+            // 
+            this.mov_acree.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.mov_acree.FillWeight = 100.179F;
+            this.mov_acree.HeaderText = "Haber";
+            this.mov_acree.MinimumWidth = 6;
+            this.mov_acree.Name = "mov_acree";
+            this.mov_acree.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.mov_acree.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // TablaIvaInventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 624);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "TablaIvaInventario";
+            this.Text = "TablaIvaInventario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn concepto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mov_deudor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mov_acree;
+    }
+}
---
+++ TransaccionesContabilidad/TransaccionesContabilidad/View/TablaIvaInventario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransaccionesContabilidad.View
+{
+    public partial class TablaIvaInventario : Form
+    {
+        public TablaIvaInventario()
+        {
+            InitializeComponent();
+            this.dataGridView1.Rows.Clear();
+            this.dataGridView1.Rows.Add(2109, "IVA DEBITO FISCAL", 0, 0);
+            this.dataGridView1.Rows.Add(1112, "IVA CREDITO FISCAL", 0, 0);
+            this.dataGridView1.Rows.Add(211103, "IVA A PAGAR", 0, 0);
+            this.dataGridView1.Rows.Add(0, "AJUSTE DE IVA", 0, 0);
+            this.dataGridView1.Rows.Add(4101, "COMPRAS", 0, 0);
+            this.dataGridView1.Rows.Add(1109, "INVENTARIO", 0, 0);
+            this.dataGridView1.Rows.Add(0, "MERCANCIA DISPONIBLE", 0, 0);
+            this.dataGridView1.Rows.Add(1109, "INVENTARIO", 0, 0);
+            this.dataGridView1.Rows.Add(4101, "COMPRAS", 0, 0);
+            this.dataGridView1.Rows.Add(0, "DETERMINAR COSTO DE VENTA", 0, 0);
+
+
+            this.dataGridView1.Refresh();
+        }
+    }
+}
