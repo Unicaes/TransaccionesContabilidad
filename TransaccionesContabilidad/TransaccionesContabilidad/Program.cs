4eda98f81597ffa7f3b457b639b9d2200221a889
Bryan Palma <bryanmauriciopalma@hotmail.com>
2020-11-12 23:04:42 -0600

    merging

--- TransaccionesContabilidad/TransaccionesContabilidad/Program.cs
+++ TransaccionesContabilidad/TransaccionesContabilidad/Program.cs
@@ -18,7 +18,6 @@ namespace TransaccionesContabilidad
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new TablaIvaInventario());
             Application.Run(new Balance());
         }
     }
--- TransaccionesContabilidad/TransaccionesContabilidad/View/TablaIvaInventario.cs
+++ TransaccionesContabilidad/TransaccionesContabilidad/View/TablaIvaInventario.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TransaccionesContabilidad.ViewModel;
 
 namespace TransaccionesContabilidad.View
 {
     public partial class TablaIvaInventario : Form
     {
-        public TablaIvaInventario()
+        private InventarioFinal invFinal;
+        public TablaIvaInventario(InventarioFinal invFinal)
         {
             InitializeComponent();
+            this.invFinal = invFinal;
+            invFinal.GetInventarioFinal();
             this.dataGridView1.Rows.Clear();
             this.dataGridView1.Rows.Add(2109, "IVA DEBITO FISCAL", 0, 0);
             this.dataGridView1.Rows.Add(1112, "IVA CREDITO FISCAL", 0, 0);
@@ -26,8 +30,6 @@ namespace TransaccionesContabilidad.View
             this.dataGridView1.Rows.Add(1109, "INVENTARIO", 0, 0);
             this.dataGridView1.Rows.Add(4101, "COMPRAS", 0, 0);
             this.dataGridView1.Rows.Add(0, "DETERMINAR COSTO DE VENTA", 0, 0);
-
-
             this.dataGridView1.Refresh();
         }
     }
